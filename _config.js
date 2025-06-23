f0819f3980f45b3daf9267de4d678816cb551b0c
GMurira <nguyaimurira@gmail.com>
2025-06-23 17:17:37 +0300

    testing the pipeline11.

--- _config.js
+++ _config.js
@@ -2,8 +2,12 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://geoffreymurira:Moringaip1@gmuriragallery.pscy2mh.mongodb.net/darkroom?retryWrites=true&w=majority&appName=GMuriraGallery',
-    development: 'mongodb+srv://geoffreymurira:Moringaip1@gmuriragallery.pscy2mh.mongodb.net/darkroom-dev?retryWrites=true&w=majority&appName=GMuriraGallery',
-    test: 'mongodb+srv://geoffreymurira:Moringaip1@gmuriragallery.pscy2mh.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=GMuriraGallery',
+    production: 'mongodb+srv://geoffreymurira:Moringaip1@gmuriragallery.pscy2mh.mongodb.net/?retryWrites=true&w=majority&appName=GMuriraGallery',
+    development: 'mongodb+srv://geoffreymurira:Moringaip1@gmuriragallery.pscy2mh.mongodb.net/?retryWrites=true&w=majority&appName=GMuriraGallery',
+    test: 'mongodb+srv://geoffreymurira:Moringaip1@gmuriragallery.pscy2mh.mongodb.net/?retryWrites=true&w=majority&appName=GMuriraGallery',
 }
-module.exports = config
+module.exports = config;
+
+
+
+//mongodb+srv://geoffreymurira:Moringaip1@gmuriragallery.pscy2mh.mongodb.net/?retryWrites=true&w=majority&appName=GMuriraGallery
